4fdfdf1f86d85d4ac5986d795fb21ae367ffbd80
nateorona98 <nathaniel.orona98@gmail.com>
2024-02-06 18:36:14 -0600

    init

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+const app = express()
+
+app.listen(5000, () => {
+    console.log('listening on port 5000')
+})
+app.get('/', (req, res) => {
+    //res.send('<p>Home Page</p>')
+    res.sendFile('./views/index.html', { root: __dirname })
+})
+app.get('/about', (req, res) => {
+    //res.send('<p>About Page</p>')
+    res.sendFile('./views/about.html', { root: __dirname })
+})
+app.get('/about-us', (req, res) => {
+    res.redirect('/about')
+})
+app.use((req, res) => {
+    res.sendFile('./views/404.html', { root: __dirname })
+})
